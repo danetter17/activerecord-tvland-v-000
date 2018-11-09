8c96c38934b4a68c9b17bd4d8e600b6f02cb514c
Daniel Etter <etter.dt@gmail.com>
2018-11-09 18:45:48 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,7 +3,7 @@ class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :network
   
-  def say_that_thing_you_always_say
+  def say_that_thing_you_say
     return "#{self.name} always says #{self.catchphrase}"
   end
 end
