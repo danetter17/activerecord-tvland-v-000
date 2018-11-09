653339bbf7be858d9ea0da7c89adfc8e28d555c5
Daniel Etter <etter.dt@gmail.com>
2018-11-09 18:43:31 +0000

    automatically backed up by learn

--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -1,4 +1,4 @@
-class CreateNetworks < ActiveRecord::Migration
+class CreateNetworks < ActiveRecord::Migration[4.2]
   #define a change method in which to do the migration
   def change
     create_table :networks do |t| #we get a block variable here for the table
